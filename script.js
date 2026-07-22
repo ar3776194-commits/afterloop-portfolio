b446ad901e5e7356ca969761979ec2f66e1d7e30
Afterloop <ar3776194@gmail.com>
2026-07-22 21:50:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// ==========================
+// AFTERLOOP
+// Designed & Developed by darkai
+// ==========================
+
+// Dynamic Copyright Year
+
+const year = document.getElementById("year");
+
+if (year) {
+
+    year.textContent = new Date().getFullYear();
+
+}
+
+// ==========================
+// Future Features
+// ==========================
+
+// Navigation
+
+// Theme Toggle
+
+// Back To Top
+
+// Animations
+
+// Contact Form
+
+// Loading Screen
