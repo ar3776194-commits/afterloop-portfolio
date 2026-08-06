ab7ce506403a6a16dfe38b549963293b139fc796
Afterloop <ar3776194@gmail.com>
2026-08-06 21:53:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,23 +2,7 @@
 // AFTERLOOP
 // Designed & Developed by darkai
 // ==========================
-if (window.location.hash === "#debug") {
-    alert("Debug mode detected");
 
-    const script = document.createElement("script");
-    script.src = "https://cdn.jsdelivr.net/npm/eruda";
-
-    script.onload = () => {
-        alert("Eruda loaded");
-        eruda.init();
-    };
-
-    script.onerror = () => {
-        alert("Eruda failed to load");
-    };
-
-    document.head.appendChild(script);
-}
 // Dynamic Copyright Year
 
 const year = document.getElementById("year");
